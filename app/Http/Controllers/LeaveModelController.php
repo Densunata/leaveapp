e98f5f4bccad94608d94ad4d54cbb9017eb3ff08
R-VICTOR\Huawei <anoreraiden4@gmail.com>
2026-02-09 15:35:46 +0800

    model staff

---
+++ app/Http/Controllers/LeaveModelController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LeaveModel;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LeaveModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LeaveModel $leaveModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LeaveModel $leaveModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, LeaveModel $leaveModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LeaveModel $leaveModel)
+    {
+        //
+    }
+}
